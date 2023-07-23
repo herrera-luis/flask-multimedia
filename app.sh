d7a3a2aa0bb1bacc86862f23946dd1aa164e2a14
herrera-luis <herrera_luis10@hotmail.com>
2023-07-23 10:00:30 -0500

    feat: updating run.sh file

--- app.sh
+++ app.sh
@@ -2,7 +2,7 @@
 source venv/bin/activate
 export PYTHONPATH="${PYTHONPATH}:${PWD}"
 export HOST="0.0.0.0"
-export PORT=9000
+export PORT=8000
 export FLASK_DEBUG=1
 export FLASK_APP=src/app.py
 flask run --host=$HOST --port=$PORT
